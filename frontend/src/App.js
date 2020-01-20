c31a1ab3ad2f231a48a0e555dff6c394946587c6
Roberto <robertomendonca20@gmail.com>
2020-01-20 13:23:10 -0300

    finished frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+import api from './service/api'
+
+import './global.css'
+import './App.css'
+import './SideBar.css'
+import './Main.css'
+
+import DevForm from './components/DevForm'
+import DevItem from './components/DevItem'
 
 function App() {
+  const [devs, setDevs] = useState([])
+
+  useEffect(() => {
+    async function loadDevs() {
+      const respose = await api.get('/devs')
+
+      setDevs(respose.data)
+    }
+
+    loadDevs()
+  }, [])
+
+  async function handleAddDev(data) {
+    const response  = await api.post('/devs', data)
+
+    setDevs([...devs, response.data])
+  }
+
   return (
-    <div className="App">
-      <p>DevRadar</p>
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <DevForm onSubmit={handleAddDev} />
+      </aside>
+
+      <main>
+        <ul>
+          {devs.map(dev => <DevItem key={dev._id} dev={dev} />)}
+        </ul>
+      </main>
     </div>
   );
 }
